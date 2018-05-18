e1f6789a90035459259f42f0faa0928ced1f1842
Anwesh43 <anweshthecool0@gmail.com>
2018-05-18 09:02:55 +0530

    adding BallContainerBoxView

--- ballcontainerboxview/src/main/java/com/example/ballcontainerboxview/BallContainerBoxView.kt
+++ ballcontainerboxview/src/main/java/com/example/ballcontainerboxview/BallContainerBoxView.kt
@@ -8,3 +8,22 @@ import android.content.*
 import android.graphics.*
 import android.view.View
 import android.view.MotionEvent
+
+class BallContainerBoxView (ctx : Context) : View(ctx) {
+
+    private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+
+}
