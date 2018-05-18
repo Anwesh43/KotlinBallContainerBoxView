9fa1cbe0e929b9d3ef375b2321ee7f75b8389579
Anwesh43 <anweshthecool0@gmail.com>
2018-05-18 09:47:27 +0530

    adding BallContainerBoxView in Activity

--- app/src/main/java/com/example/anweshmishra/kotlinballcontainerboxview/MainActivity.kt
+++ app/src/main/java/com/example/anweshmishra/kotlinballcontainerboxview/MainActivity.kt
@@ -2,10 +2,12 @@ package com.example.anweshmishra.kotlinballcontainerboxview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.example.ballcontainerboxview.BallContainerBoxView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        BallContainerBoxView.create(this)
     }
 }
